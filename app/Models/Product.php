46053088f69aa930a1f90d3723f427a55e89b425
eman ouda <eman@gmail.com>
2025-10-26 11:19:49 +0200

    make proudut model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,31 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'product';
+
+ 
+    protected $fillable = [
+        'name',
+        'category_id',
+        'description',
+        'quantity',
+        'price',
+        'color',
+        'size',
+        'image',
+    ];
+
+  
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
