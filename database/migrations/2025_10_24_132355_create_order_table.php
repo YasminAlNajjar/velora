631a0c28d9a765c5b246b38498426f51fa17b947
YasminAlNajjar <y3sm2004@gmail.com>
2025-10-26 09:28:47 +0200

    First commit

---
+++ database/migrations/2025_10_24_132355_create_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->string('user_phone');
+            $table->string('user_address');
+            $table->string('status');
+            $table->decimal('total_price',8,2);
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
