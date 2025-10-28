d76f3cf6ddb8d998ec7d837615a888ae6a139c81
YasminAlNajjar <y3sm2004@gmail.com>
2025-10-28 08:37:14 +0200

    Add Controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemsController;
+use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ReviewController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+//******************Main************************ */
+
+
+//******************Category************************ */
+Route::get('',[CategoryController::class,'']);
+
+
+//******************Product************************ */
+
+
+//******************Order************************ */
+
+
+//******************Order Items************************ */
+
+
+//******************Discount************************ */
+
+
+//******************Cart************************ */
+
+
+//******************Review************************ */
